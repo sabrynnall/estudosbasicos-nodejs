07b9256b0e48a7302d53d871a0b5bd6f7d0d7a96
Sabrynna Lourenço <86985496+sabrynnall@users.noreply.github.com>
2021-09-20 14:56:29 -0300

    subindo estudo sobre array

---
+++ diasDoMes.js
@@ -0,0 +1,9 @@
+var mes = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Março', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+var dia = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    for(i = 0; i < mes.length; i++){
+        console.log(`O mês de ${mes[i]} tem ${dia[i]} dias`);
+    }
+
+
+
